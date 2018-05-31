cc3f05beb8e1f12cbc21360aaae8dbd6117b5d02
lijianzhen5 <lijianzhen1@jd.com>
2018-05-31 13:54:18 +0800

    简单使用spring-boot的结构。提交eureka的分布式协调

--- sc-demo-web/src/main/java/com/xn/demo/ProciderUserApplication.java
+++ sc-demo-web/src/main/java/com/xn/demo/ProciderUserApplication.java
@@ -1,13 +1,15 @@
-package demo;
+package com.xn.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author lijizhen1@jd.com
  * @date 2018/5/30 20:26
  */
 @SpringBootApplication
+@ComponentScan("com.xn.demo")
 public class ProciderUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProciderUserApplication.class, args);
