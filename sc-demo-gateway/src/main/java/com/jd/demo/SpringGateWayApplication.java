e3b681d12835b8d1e217bdf803b941e22538ec53
Janle <janle@clouds1000.com>
2022-09-21 10:12:00 +0800

    网关测试的demo

--- sc-demo-gateway/src/main/java/com/jd/demo/SpringGateWayApplication.java
+++ sc-demo-gateway/src/main/java/com/jd/demo/SpringGateWayApplication.java
@@ -1,7 +1,14 @@
 package com.jd.demo;
 
+import com.sun.net.httpserver.HttpServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+
+import java.net.InetSocketAddress;
 
 /**
  * @author Janle on 2022/3/31
@@ -9,6 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringGateWayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringGateWayApplication.class, args);
+        ConfigurableApplicationContext app= SpringApplication.run(SpringGateWayApplication.class, args);
+
+        //通过ApplicationContext创建HttpHandler
+        HttpHandler httpHandler = WebHttpHandlerBuilder.applicationContext(app).build();
+        ReactorHttpHandlerAdapter httpHandlerAdapter = new ReactorHttpHandlerAdapter(httpHandler);
+       // HttpServer.create(new InetSocketAddress("localhost",8080),0).newHandler(httpHandlerAdapter).block();
     }
 }
